42fdc208c5c28fbf426d7dcb6a2ed47cf674bceb
AleksandrukV <valentynaleksandruk2005@gmail.com>
2024-03-12 19:33:08 +0200

    Added the code to the file[Task3]

--- Home-work_1/Task3.cpp
+++ Home-work_1/Task3.cpp
@@ -1,1 +1,25 @@
 #include"Library.h"
+
+void CaesarEncryption(FILE* f1,FILE * f2)
+{
+	char c = fgetc(f1);
+	int key = 2;
+	while (c != EOF)
+	{
+		if (c != '\n')
+		{
+			fputc(c + key, f2);
+		}
+		else
+			fputc(c, f2);
+		c = fgetc(f1);
+	}
+}
+
+int main()
+{
+	FILE* f1 = fopen("D:\\temp\\file.txt", "r");
+	FILE* f2 = fopen("D:\\temp\\file2.txt", "w");
+	CaesarEncryption(f1, f2);
+	fclose(f1);
+}
